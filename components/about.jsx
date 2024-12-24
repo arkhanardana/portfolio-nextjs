9a5e701d0e4943f550b3bbae669499a0a56e54d9
arkhanardana <hanzmantap@gmail.com>
2024-12-24 21:07:35 +0700

    edit about me

--- components/about.jsx
+++ components/about.jsx
@@ -5,7 +5,7 @@ export default function About() {
 	return (
 		<div className="container mx-auto">
 			<div className="flex mx-16 my-10 justify-between items-center">
-				<BlurIn word={"About Me"} className={"text-4xl font-bold md:text-7xl"}></BlurIn>
+				<BlurIn word={"About Me"} className={"text-2xl sm:text-4xl font-bold md:text-7xl"}></BlurIn>
 
 				<div className="inline-flex items-center gap-2 rounded-full bg-gray-100/80 px-4 py-4">
 					<Globe className="h-10 w-10 text-gray-600" strokeWidth={1.5} />
