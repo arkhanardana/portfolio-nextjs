db1e67347a5940feca3e374e9bafcc54ff16203f
arkhanardana <hanzmantap@gmail.com>
2025-04-09 22:10:53 +0700

    feat: add meta tag for google search console

--- app/layout.jsx
+++ app/layout.jsx
@@ -29,6 +29,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className="selection:bg-slate-800 selection:text-white">
       <link rel="icon" href="/favicon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="google-site-verification" content="vddL4RIMEBuMyzipQ4Rlv-rQrepggxLZ-zDMNEELlHk" />
       <body className={`${font.className}`}>
         <LenisProvider />
         {children}
